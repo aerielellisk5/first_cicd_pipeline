62644b66249204ad89bc54e62fba427e4a524d19
Aeriel <aerielellisk5@gmail.com>
2023-06-11 15:57:56 -0700

    just getting the baseline of the project setup with surge and prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// CI=true npm run test ---- will run only the tests and not listen for the changes in the file
+
+// CI=true npm run test -- --coverage will generate a test report in the browser
+
+// npm run build -- will build an optimized bundle ready for production
+
+// domain to use for surge: truculent-profit.surge.sh
