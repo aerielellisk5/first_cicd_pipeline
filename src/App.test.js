d706b74cb9b68131fe5ad56fa901230ebe5a9399
Aeriel <aerielellisk5@gmail.com>
2023-06-11 22:25:28 -0700

    added prettier formatting2

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-// CI=true npm run test ---- will run only the tests and not listen for the changes in the file
-
-// CI=true npm run test -- --coverage will generate a test report in the browser
-
-// npm run build -- will build an optimized bundle ready for production
-
-// domain to use for surge: truculent-profit.surge.sh
-// you can run 'surge list' and that will show the list of domains that are generated on your machine
