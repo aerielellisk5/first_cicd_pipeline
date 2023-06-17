b037873de935c8bc73d16df9f94ff6707cb7d464
Aeriel <aerielellisk5@gmail.com>
2023-06-17 11:21:41 -0700

    fix: fix description1

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -3,3 +3,5 @@ module.exports  = {
     repositoryUrl: "https://github.com/aerielellisk5/first_cicd_pipeline",
     plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
 }
+
+// extra here to try again
